7d6e8821ef9af247905bed6c6614c9c9b339cad2
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-07-26 23:45:00 +0530

    Refactor Blackjack Game: update HTML structure, enhance JavaScript logic for card handling, and improve CSS styling for buttons

--- 03-blackjack-game/script.js
+++ 03-blackjack-game/script.js
@@ -1,14 +1,37 @@
-let firstCard = 15;
-let secondCard = 11;
-let sum = firstCard + secondCard;
+cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 // let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("card-el");
+
+function getRandomCard() {
+  let random = Math.floor(Math.random() * 13) + 1;
+  if (random === 1) {
+    return 11;
+  } else if (random > 10) {
+    return 10;
+  }
+  return random;
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = `Cards: `;
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += `${cards[i]} `;
+  }
   sumEl.textContent = `Sum: ${sum}`;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -21,4 +44,14 @@ function startGame() {
   }
 
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
