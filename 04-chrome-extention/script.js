117eed2680dad14a6734e530edd5c5345f8f395e
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-01 23:15:11 +0530

    Refactor lead management: initialize myLeads as an empty array and update renderLeads function for improved performance

--- 04-chrome-extention/script.js
+++ 04-chrome-extention/script.js
@@ -1,13 +1,27 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  // console.log(myLeads)
+  inputEl.value = ""
+  renderLeads();
+
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+function renderLeads(){
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+  // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+  listItems += `<li>${myLeads[i]}</li>`;
+  // const li = document.createElement("li");
+  //   li.textContent = myLeads[i]
+  //   ulEl.append(li);
 }
+
+// console.log(listItems);
+ulEl.innerHTML = listItems;
+}
+
